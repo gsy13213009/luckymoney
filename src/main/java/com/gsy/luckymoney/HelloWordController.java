e4d3412549c1ab5a0c8a11297e3860b5a77cfe61
Siyi Guo <siyi.guo@grabtaxi.com>
2020-04-12 11:29:18 +0800

    (CHORE) 使用类解析配置文件的变量

--- src/main/java/com/gsy/luckymoney/HelloWordController.java
+++ src/main/java/com/gsy/luckymoney/HelloWordController.java
@@ -1,21 +1,17 @@
 package com.gsy.luckymoney;
 
-import java.math.BigDecimal;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWordController {
 
-    @Value("${minMoney}")
-    private BigDecimal minMoney;
-
-    @Value("${description}")
-    private String description;
+    @Autowired
+    LimitConfig mLimitConfig;
 
     @GetMapping("/hello")
     public String say() {
-        return "minMoney:" + minMoney + ", 说明：" + description;
+        return mLimitConfig.getDescription();
     }
 }
