8305e5954a5a22444093a8ffb8390be8c4c9e7fc
Siyi Guo <siyi.guo@grabtaxi.com>
2020-04-12 18:35:30 +0800

    (CHORE) 使用slf4j打印log:

--- src/main/java/com/gsy/luckymoney/aspect/HttpAspect.java
+++ src/main/java/com/gsy/luckymoney/aspect/HttpAspect.java
@@ -5,11 +5,15 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class HttpAspect {
+
+    private final static Logger logger = LoggerFactory.getLogger(HttpAspect.class);
 
     // 优化代码，使用Pointcut定义log方法，避免多次写execution
     @Pointcut("execution(public * com.gsy.luckymoney.controller.GirlController.*(..))")
@@ -21,13 +25,13 @@ public class HttpAspect {
 //    @Before("execution(public * com.gsy.luckymoney.controller.GirlController.*(..))")
     @Before("log()")
     public void doBefore() {
-        System.out.println("hhhhhhhhhhhhh");
+        logger.info("ffffffffffff");
     }
 
     // Before方法执行之前就执行该方法了
 //    @After("execution(public * com.gsy.luckymoney.controller.GirlController.*(..))")
     @After("log()")
     public void doAfter() {
-        System.out.println("hhhhhhhhhhhhh");
+        logger.info("hhhhhhhhhhhhhh");
     }
 }
