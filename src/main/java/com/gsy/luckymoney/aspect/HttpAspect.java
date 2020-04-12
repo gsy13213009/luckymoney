0d5fe6069b6a86383672c33c4c6014a73f74a1dd
Siyi Guo <siyi.guo@grabtaxi.com>
2020-04-12 18:32:16 +0800

    (CHORE) 优化代码，使用Pointcut定义log方法，避免多次写execution

--- src/main/java/com/gsy/luckymoney/aspect/HttpAspect.java
+++ src/main/java/com/gsy/luckymoney/aspect/HttpAspect.java
@@ -4,20 +4,29 @@ package com.gsy.luckymoney.aspect;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class HttpAspect {
 
+    // 优化代码，使用Pointcut定义log方法，避免多次写execution
+    @Pointcut("execution(public * com.gsy.luckymoney.controller.GirlController.*(..))")
+    public void log() {
+
+    }
+
     // Before方法执行之前就执行该方法了
-    @Before("execution(public * com.gsy.luckymoney.controller.GirlController.*(..))")
+//    @Before("execution(public * com.gsy.luckymoney.controller.GirlController.*(..))")
+    @Before("log()")
     public void doBefore() {
         System.out.println("hhhhhhhhhhhhh");
     }
 
     // Before方法执行之前就执行该方法了
-    @After("execution(public * com.gsy.luckymoney.controller.GirlController.*(..))")
+//    @After("execution(public * com.gsy.luckymoney.controller.GirlController.*(..))")
+    @After("log()")
     public void doAfter() {
         System.out.println("hhhhhhhhhhhhh");
     }
