7475e92abfec2473961bbfb27da8dbeb81d061f6
Siyi Guo <siyi.guo@grabtaxi.com>
2020-04-12 12:04:36 +0800

    (CHORE) 为请求添加前缀

--- src/main/java/com/gsy/luckymoney/HelloWordController.java
+++ src/main/java/com/gsy/luckymoney/HelloWordController.java
@@ -3,9 +3,11 @@ package com.gsy.luckymoney;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("hello")
 public class HelloWordController {
 
     @Autowired
@@ -17,7 +19,7 @@ public class HelloWordController {
     }
 
 
-    @GetMapping("/hello2")
+    @GetMapping({"/hello2", "hi"})
     @ResponseBody
     public String say2() {
         return mLimitConfig.getDescription();
