f9f773146ed1a922b91e267849ecf40461957b2d
Siyi Guo <siyi.guo@grabtaxi.com>
2020-04-12 11:14:51 +0800

    (CHORE) 添加数据字段，添加yaml文件，在controller里面使用yaml里面的字段

--- src/main/java/com/gsy/luckymoney/HelloWordController.java
+++ src/main/java/com/gsy/luckymoney/HelloWordController.java
@@ -1,13 +1,21 @@
 package com.gsy.luckymoney;
 
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWordController {
 
+    @Value("${minMoney}")
+    private BigDecimal minMoney;
+
+    @Value("${description}")
+    private String description;
+
     @GetMapping("/hello")
     public String say() {
-        return "hhhhhhhhhh";
+        return "minMoney:" + minMoney + ", 说明：" + description;
     }
 }
