9cd62d33c213748c385ec65e7e677306bfab6b40
Siyi Guo <siyi.guo@grabtaxi.com>
2020-04-12 12:09:21 +0800

    (CHORE) 添加路径中的id获取id的方式

--- src/main/java/com/gsy/luckymoney/HelloWordController.java
+++ src/main/java/com/gsy/luckymoney/HelloWordController.java
@@ -3,6 +3,7 @@ package com.gsy.luckymoney;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,9 +20,9 @@ public class HelloWordController {
     }
 
 
-    @GetMapping({"/hello2", "hi"})
+    @GetMapping("/hello/{id}")
     @ResponseBody
-    public String say2() {
-        return mLimitConfig.getDescription();
+    public String say2(@PathVariable("id") Integer id) {
+        return "id: " + id;
     }
 }
