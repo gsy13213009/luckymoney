6b76254d7dbed62685588df819a64770c704468a
Siyi Guo <siyi.guo@grabtaxi.com>
2020-04-12 19:05:03 +0800

    (CHORE) 在HttpAspect里面，对请求前，请求后以及return的值做log输出

--- src/main/java/com/gsy/luckymoney/aspect/HttpAspect.java
+++ src/main/java/com/gsy/luckymoney/aspect/HttpAspect.java
@@ -1,13 +1,18 @@
 package com.gsy.luckymoney.aspect;
 
 
+import javax.servlet.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Aspect
 @Component
@@ -24,8 +29,21 @@ public class HttpAspect {
     // Before方法执行之前就执行该方法了
 //    @Before("execution(public * com.gsy.luckymoney.controller.GirlController.*(..))")
     @Before("log()")
-    public void doBefore() {
-        logger.info("ffffffffffff");
+    public void doBefore(JoinPoint joinPoint) {
+
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        // url
+        logger.info("url={}", request.getRequestURL());
+        // method
+        logger.info("method={}", request.getMethod());
+        // ip
+        logger.info("ip={}", request.getRemoteAddr());
+        // 类方法
+        logger.info("class_method={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        // 参数
+        logger.info("args={}", joinPoint.getArgs());
+
     }
 
     // Before方法执行之前就执行该方法了
@@ -33,5 +51,11 @@ public class HttpAspect {
     @After("log()")
     public void doAfter() {
         logger.info("hhhhhhhhhhhhhh");
+    }
+
+    @AfterReturning(returning = "object", pointcut = "log()")
+    public void doAfterReturning(Object object) {
+        // 打印出返回信息
+        logger.info(object.toString());
     }
 }
