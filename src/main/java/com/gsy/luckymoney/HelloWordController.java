07477df841e1e77554cff171fa2731fea8ceea96
Siyi Guo <siyi.guo@grabtaxi.com>
2020-04-12 11:52:05 +0800

    (CHORE) @Controller + @ResponseBody = @RestController

--- src/main/java/com/gsy/luckymoney/HelloWordController.java
+++ src/main/java/com/gsy/luckymoney/HelloWordController.java
@@ -3,6 +3,7 @@ package com.gsy.luckymoney;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWordController {
@@ -12,7 +13,13 @@ public class HelloWordController {
 
     @GetMapping("/hello")
     public String say() {
-//        return mLimitConfig.getDescription();
         return "index";
+    }
+
+
+    @GetMapping("/hello2")
+    @ResponseBody
+    public String say2() {
+        return mLimitConfig.getDescription();
     }
 }
